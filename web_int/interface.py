92d938c5ff080f2cb2c962503ac43c27238966eb
robotafm <robotafm@gmail.com>
2021-02-25 11:08:21 +0300

    Add main web interface script

---
+++ web_int/interface.py
@@ -0,0 +1,20 @@
+# /robotafm/Capsule/web_int/interface.py
+# Main web interface, contains basic
+# functions information display
+
+def server_start(port=80):
+    """
+    Запуск сервера для отображения информации
+    port: Номер порта, на котором запускается сервер
+    """
+    pass #do nothing
+
+def server_stop():
+    """
+    Останавливает запущенный сервер
+    """
+    pass #do nothing
+
+server_start(777)
+server_stop()
+
