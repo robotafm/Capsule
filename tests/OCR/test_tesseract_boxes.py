073548c369984bf3bf53a27ff4fffc67581a4572
robotafm <robotafm@gmail.com>
2021-03-11 16:30:01 +0300

    Add ALTO XML to test_tesseract_boxes.py

--- tests/OCR/test_tesseract_boxes.py
+++ tests/OCR/test_tesseract_boxes.py
@@ -3,7 +3,8 @@ import pytesseract
 
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
-filename = 'tmp/tests.png'
+filename = r'D:\Data\testdata\img\OCR\tests.png'
+file_alto_xml = r'D:\Data\testdata\img\OCR\test_alto.xml'
 
 # read the image and get the dimensions
 img = cv2.imread(filename)
@@ -12,6 +13,17 @@ h, w, _ = img.shape # assumes color image
 # run tesseract, returning the bounding boxes
 boxes = pytesseract.image_to_boxes(img) #use
 print(pytesseract.image_to_string(img, lang='rus+eng')) #print identified text
+
+osd = pytesseract.image_to_osd(img);
+print("<<<<  OSD  >>>>", osd)
+
+# Специальный формат xml для распознанной страницы
+alto_xml = pytesseract.image_to_alto_xml(img, lang='rus+eng')
+
+# Сохраняем в файл (перезапись, байтово)
+f = open(file_alto_xml, "wb")
+f.write(alto_xml)
+f.close()
 
 # draw the bounding boxes on the image
 for b in boxes.splitlines():
